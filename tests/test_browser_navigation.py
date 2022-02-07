a365c665a4d84b49b395aafb91e8416ef9d1b191
Guy H <guybkh@gmail.com>
2022-02-07 00:23:46 -0600

    Report 4 - Browser Navigation script.

---
+++ tests/test_browser_navigation.py
@@ -0,0 +1,25 @@
+# Report 2 - Automation Script Ran w/Driver in PATH
+
+# Import Libraries
+import pytest
+from selenium import webdriver
+
+
+def test_browser_navigation():
+    # Create Driver Instance & Launch Chrome Browser
+    driver = webdriver.Chrome()
+
+    # Navigating to a website
+    driver.get("https://selenium.dev")
+
+    # Pressing the Back button
+    driver.back()
+
+    # Pressing the Forward button
+    driver.forward()
+
+    # Refreshing the page
+    driver.refresh()
+
+    # Closing the window
+    driver.quit()
